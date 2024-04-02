2c2d42cd4f01e6370508ac37d352c96318e42777
i-yuki23 <yuki.ito.s7@dc.tohoku.ac.jp>
2024-04-02 10:56:07 +0900

    added 404 page not found

--- src/Application.php
+++ src/Application.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__ . '/core/Router.php';
+require_once __DIR__ . '/core/HttpNotFoundException.php';
 require_once __DIR__ . '/controller/ShuffleController.php';
 require_once __DIR__ . '/controller/EmployeeController.php';
 
@@ -15,15 +16,25 @@ class Application
 
     public function run()
     {   
-        $params = $this->router->resolve($this->getPathInfo());
-        $controller = $params['controller'];
-        $action = $params['action'];
-        $this->runAction($controller, $action);
+        try {
+            $params = $this->router->resolve($this->getPathInfo());
+            if (!$params) {
+                throw new HttpNotFoundException();
+            }
+            $controller = $params['controller'];
+            $action = $params['action'];
+            $this->runAction($controller, $action);
+        } catch (HttpNotFoundException) {
+            $this->render404Page();
+        }
     }
 
     private function runAction($controllerName, $action)
     {
         $controllerClass = ucfirst($controllerName) . 'Controller';
+        if (!class_exists($controllerClass)) {
+            throw new HttpNotFoundException();
+        }
         $controller = new $controllerClass();
         $controller->run($action);
     }
@@ -41,5 +52,26 @@ class Application
     private function getPathInfo()
     {
         return $_SERVER['REQUEST_URI'];
+    }
+
+    private function render404Page()
+    {
+        header('HTTP/1.1 404 Page Not Found');
+        $content = <<<EOF
+<!DOCTYPE html>
+<head>
+    <!-- <meta charset="utf-8"> -->
+    <meta name="viewpoint" content="width=device-width, initial-scale=1">
+    <!-- <link rel="stylesheet" href="stylesheets/css/app.css"> -->
+    <title>Shuffle Lunch</title>
+</head>
+<body>
+    <h2>
+        404 Page Not Found.
+    </h2>
+</body>
+</html>
+EOF;
+        echo $content;
     }
 }
